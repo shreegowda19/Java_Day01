3ee97db633922f592d936ad7dcc517e86373c60b
shreegowda19 <shreegowda190207@gmail.com>
2026-08-20 17:36:45 +0530

    Add files via upload

---
+++ programm4.java
@@ -0,0 +1,46 @@
+public import java.util.Scanner;
+
+class Student {
+    String name;
+    int age;
+    int rollNo;
+    int marks;
+
+    void input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Enter Age: ");
+        age = sc.nextInt();
+
+        System.out.print("Enter Roll No: ");
+        rollNo = sc.nextInt();
+
+        System.out.print("Enter Marks: ");
+        marks = sc.nextInt();
+    }
+
+    void display() {
+        System.out.println("\n----- Student Details -----");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Marks: " + marks);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Create Student object
+        Student s = new Student();
+
+        // Call input method
+        s.input();
+
+        // Call display method
+        s.display();
+    }
+} 
